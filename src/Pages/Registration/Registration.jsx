864a05e42af882ac093209c0fb9fa9f6c1aaaf26
01subhashree <ssubhashree094@gmail.com>
2023-04-22 18:12:53 +0530

    final project

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -61,9 +61,11 @@ function Registration() {
           label="First Name"
           variant="filled"
           helperText={
-            /^[a-zA-Z]+$/.test(fname)
-              ? "Valid Name"
-              : "Only contains Alphabets "
+            /^[a-zA-Z]+$/.test(fname) ? (
+              <span>Valid Name</span>
+            ) : (
+              <span>Only contains Alphabets </span>
+            )
           }
           onChange={(e) => {
             setFname(e.target.value);
@@ -82,9 +84,11 @@ function Registration() {
           variant="filled"
           title="LastName"
           helperText={
-            /^[a-zA-Z]+$/.test(lname)
-              ? " Valid Name"
-              : "Only contains Alphabets"
+            /^[a-zA-Z]+$/.test(lname) ? (
+              <span>Valid Name</span>
+            ) : (
+              <span>Only contains Alphabets</span>
+            )
           }
           onChange={(e) => {
             setLname(e.target.value);
@@ -104,9 +108,11 @@ function Registration() {
           label="E-mail"
           variant="filled"
           helperText={
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
-              ? "Valid email"
-              : "Enter Valid Email"
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) ? (
+              <span>Valid email</span>
+            ) : (
+              <span> Enter Valid Email</span>
+            )
           }
           onChange={(e) => {
             setEmail(e.target.value);
@@ -125,9 +131,11 @@ function Registration() {
           title="Password"
           variant="filled"
           helperText={
-            /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/.test(password)
-              ? "Strong Password"
-              : " Password contain 8 characters with one number"
+            /^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/.test(password) ? (
+              <span>Strong Password</span>
+            ) : (
+              <span>Password contain 8 characters with one number</span>
+            )
           }
           onChange={(e) => {
             setPassword(e.target.value);
